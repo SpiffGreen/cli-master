5fd3795fd9e1958f543c6051dee8518b19cbafea
SpiffGreen <spiffjekeygreen@gmail.com>
2021-08-16 00:35:23 +0100

    Improved the trimming functions: Added trimming by depth feature

--- util.h
+++ util.h
@@ -14,9 +14,9 @@ template <typename T>
 void println(T);
 
 std::string input(std::string);
-std::string trimLeft(std::string, char);
-std::string trimRight(std::string, char);
-std::string trim(std::string, char);
+std::string trimLeft(std::string, char, int);
+std::string trimRight(std::string, char, int);
+std::string trim(std::string, char, int);
 bool startsWith(std::string, std::string);
 
 
@@ -38,24 +38,33 @@ std::string input(std::string question) {
     return answer;
 }
 
-std::string trimLeft(std::string str, char delimeter = ' ') {
+std::string trimLeft(std::string str, char delimeter = ' ', int depth = 0) {
     int i;
     for(i = 0; i < str.size(); i++) {
         if(str[i] != delimeter) break;
+        if(depth != 0) {
+            if(i >= depth) break;
+        }
     }
     return str.substr(i);
 }
 
-std::string trimRight(std::string str, char delimeter = ' ') {
+std::string trimRight(std::string str, char delimeter = ' ', int depth = 0) {
     int i;
+    bool defaultDepth = false;
+    if(depth == 0) defaultDepth = true;
     for(i = str.size() - 1; i >= 0; i--) {
         if(str[i] != delimeter) break;
+        if(depth != 0) depth--;
+        else {
+            if(!defaultDepth) break;
+        }
     }
     return str.substr(0, i + 1);
 }
 
-std::string trim(std::string str, char delimeter = ' ') {
-    return trimLeft(trimRight(str, delimeter), delimeter);
+std::string trim(std::string str, char delimeter = ' ', int depth = 0) {
+    return trimLeft(trimRight(str, delimeter, depth), delimeter, depth);
 }
 
 bool startsWith(std::string str, std::string ch) {
